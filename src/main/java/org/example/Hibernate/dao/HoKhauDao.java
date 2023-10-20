e527d5a4513ebbfb4e34c105e8d675d8d570bca9
hoang-mai <maianhhoang31072003@gmail.com>
2023-10-20 16:11:19 +0700

    làm tiếp đi thg nghiện

---
+++ src/main/java/org/example/Hibernate/dao/HoKhauDao.java
@@ -0,0 +1,31 @@
+package org.example.Hibernate.dao;
+
+import org.example.EntityAll.HoKhau;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.io.Serializable;
+
+public class HoKhauDao {
+ private SessionFactory SessionFactory=null;
+ private  Session session=null;
+
+    public static HoKhauDao getInstance() {
+        return new HoKhauDao();
+    }
+
+    public boolean save(HoKhau hoKhau){
+       try {
+           SessionFactory=Hibernate.getSessionFactory();
+           session=Hibernate.getSession(SessionFactory);
+           Serializable serializable= (Serializable) session.save(hoKhau);
+           Hibernate.closeSession(session);
+           Hibernate.closeSessionFactory(SessionFactory);
+           return (serializable!=null);
+       } catch (Exception e) {
+           System.out.println("Luu ho khau co loi");
+           throw new RuntimeException(e);
+       }
+    }
+
+}
