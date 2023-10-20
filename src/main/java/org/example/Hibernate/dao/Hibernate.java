60012deaa631928dc111e1223f7ed4d3ba954441
J Johnson <dung060903@gmail.com>
2023-10-20 15:46:17 +0700

    add

--- src/main/java/org/example/Hibernate/dao/Hibernate.java
+++ src/main/java/org/example/Hibernate/dao/Hibernate.java
@@ -4,15 +4,23 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 public class Hibernate {
-    public static void main(String[] args) {
+    public static SessionFactory getSessionFactory() {
         SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        return sessionFactory;
+    }
 
+    public static Session getSession(SessionFactory sessionFactory) {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
+        return  session;
+    }
 
+    public static void closeSession(Session session) {
         session.getTransaction().commit();
         session.close();
+    }
 
+    public static void closeSessionFactory(SessionFactory sessionFactory) {
         sessionFactory.close();
     }
 }
