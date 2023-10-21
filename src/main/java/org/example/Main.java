c63df754210e20151609fd10eb854d5ecdda300d
pinocchioo1412 <oulive1412@gmail.com>
2023-10-21 19:22:27 +0700

    loo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+double x,y = 0;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("DashBoard.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
