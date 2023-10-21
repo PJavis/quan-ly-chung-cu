7a7189cd6c54e56428bfc4b8180613fa9b3f12bb
hoang-mai <maianhhoang31072003@gmail.com>
2023-10-21 23:14:57 +0700

    làm tiếp đi thg nghiện

--- src/main/java/org/example/ConTroller/DashBoardConTroller.java
+++ src/main/java/org/example/ConTroller/DashBoardConTroller.java
@@ -10,13 +10,28 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 public class DashBoardConTroller implements Initializable {
+    @FXML
+    private Pane tinhnang;
 
-
+    private boolean isDashboardVisible = false;
     @FXML
     void hiendashbroad(ActionEvent event) {
-
+        if (isDashboardVisible) {
+            // Ẩn bảng
+            TranslateTransition closeTransition = new TranslateTransition(Duration.seconds(0.5), tinhnang);
+            closeTransition.setToX(0);
+            closeTransition.play();
+            isDashboardVisible = false;
+        } else {
+            // Hiển thị bảng
+            TranslateTransition openTransition = new TranslateTransition(Duration.seconds(0.5), tinhnang);
+            openTransition.setToX(240);
+            openTransition.play();
+            isDashboardVisible = true;
+        }
     }
 
     @FXML
