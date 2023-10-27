a28cf580990d3fbc5c7fa6e80d6fdbb64f9771ac
hoang-mai <maianhhoang31072003@gmail.com>
2023-10-28 02:51:04 +0700

    làm chill5

--- src/main/java/org/example/ConTroller/TaonhankhauController.java
+++ src/main/java/org/example/ConTroller/TaonhankhauController.java
@@ -1,4 +1,52 @@
 package org.example.ConTroller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.example.EntityAll.NhanKhau;
+
 public class TaonhankhauController {
+
+    @FXML
+    private CheckBox co;
+
+    @FXML
+    private Label gioitinh;
+
+    @FXML
+    private CheckBox khong;
+
+    @FXML
+    private TextField ngaysinh;
+
+    @FXML
+    private TextField quoctich;
+
+    @FXML
+    private TextField sophong;
+
+    @FXML
+    private TextField sotien;
+
+    @FXML
+    private TextField tennhankhau;
+
+    @FXML
+    void huy(ActionEvent event) {
+        Stage a = (Stage) sophong.getScene().getWindow();
+        a.close();
+    }
+
+    @FXML
+    void taomoi(ActionEvent event) {
+        NhanKhau nhanKhau=new NhanKhau();
+        nhanKhau.setTen(tennhankhau.getText());
+        nhanKhau.getNgaySinh();
+        Stage a = (Stage) sophong.getScene().getWindow();
+        a.close();
+    }
+
 }
--- src/main/java/org/example/ConTroller/TaophongController.java
+++ src/main/java/org/example/ConTroller/TaophongController.java
@@ -1,4 +1,34 @@
 package org.example.ConTroller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.example.EntityAll.HoKhau;
+import org.example.Hibernatedao.HoKhauDao;
+
 public class TaophongController {
+
+    @FXML
+    private TextField dientichphong;
+
+    @FXML
+    private TextField sophong;
+
+    @FXML
+    void huy(ActionEvent event) {
+        Stage a = (Stage) sophong.getScene().getWindow();
+        a.close();
+    }
+
+    @FXML
+    void taomoi(ActionEvent event) {
+        HoKhau hoKhau=new HoKhau();
+        hoKhau.setId(Integer.parseInt(sophong.getText()));
+        hoKhau.setDienTichPhong(Double.parseDouble(dientichphong.getText()));
+        HoKhauDao.getInstance().save(hoKhau);
+        Stage a = (Stage) sophong.getScene().getWindow();
+        a.close();
+    }
+
 }
