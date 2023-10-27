737ce4a4af25bee7841dec15b13e369758694a01
hoang-mai <maianhhoang31072003@gmail.com>
2023-10-28 00:04:36 +0700

    làm chill4

--- src/main/java/org/example/ConTroller/DashBoardController.java
+++ src/main/java/org/example/ConTroller/DashBoardController.java
@@ -83,6 +83,39 @@ public class DashBoardController {
             System.out.println(e.getMessage());
         }
     }
+    @FXML
+    void dieuchinhdancu(ActionEvent event) {
+        try {
+            Stage ag0r1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org.example/Dieuchinh.fxml"));
+            Parent root = loader.load();
+            TabPane newTabPane = (TabPane) root.lookup("#tabpanedieuchinh");
+            Tab desiredTab = newTabPane.getTabs().get(1);
+            Scene scene = new Scene(root);
+            ag0r1.setScene(scene);
+            ag0r1.show();
+            newTabPane.getSelectionModel().select(desiredTab);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    void dieuchinhphong(ActionEvent event) {
+        try {
+            Stage ag0r1 = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org.example/Dieuchinh.fxml"));
+            Parent root = loader.load();
+            TabPane newTabPane = (TabPane) root.lookup("#tabpanedieuchinh");
+            Tab desiredTab = newTabPane.getTabs().get(2);
+            Scene scene = new Scene(root);
+            ag0r1.setScene(scene);
+            ag0r1.show();
+            newTabPane.getSelectionModel().select(desiredTab);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @FXML
     void thongkecackhoanphi(ActionEvent event) {
---
+++ src/main/java/org/example/ConTroller/TaonhankhauController.java
@@ -0,0 +1,4 @@
+package org.example.ConTroller;
+
+public class TaonhankhauController {
+}
