d3a98719aa54ce4e387d8007aa9765ea7df94e3d
hoang-mai <maianhhoang31072003@gmail.com>
2023-10-28 20:50:29 +0700

    làm chill6

--- src/main/java/org/example/ConTroller/TaonhankhauController.java
+++ src/main/java/org/example/ConTroller/TaonhankhauController.java
@@ -2,6 +2,8 @@ package org.example.ConTroller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -10,11 +12,16 @@ import org.example.EntityAll.NhanKhau;
 import org.example.Hibernatedao.HoKhauDao;
 import org.example.Hibernatedao.NhanKhauDao;
 
+import java.net.URL;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class TaonhankhauController {
+public class TaonhankhauController implements Initializable {
+    private boolean hienbuttontaomoi=false;
+    @FXML
+    private Button buttontaomoi;
 
     @FXML
     private CheckBox co;
@@ -55,10 +62,28 @@ public class TaonhankhauController {
         if(co.isSelected()){
             nhanKhau.setChuHo(true);
         }else nhanKhau.setChuHo(false);
+        nhanKhau.setHoKhau(HoKhauDao.getInstance().selectById(Integer.parseInt(sophong.getText())));
 nhanKhau.setTrangThai("Đang ở");
         NhanKhauDao.getInstance().save(nhanKhau);
         Stage a = (Stage) sophong.getScene().getWindow();
         a.close();
     }
+    private void checkAllFieldsFilled(TextField[] textFields) {
+        boolean allFilled = true;
+        for (TextField textField : textFields) {
+            if (textField.getText().isEmpty()) {
+                allFilled = false;
+                break;
+            }
+        }
+        buttontaomoi.setDisable(!allFilled);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TextField[] textFields={tennhankhau,sophong,quoctich,ngaysinh,gioitinh};
+        for (TextField textField : textFields) {
+            textField.textProperty().addListener((observable, oldValue, newValue) -> checkAllFieldsFilled(textFields));
+        }
 
+    }
 }
--- src/main/java/org/example/ConTroller/TaophiController.java
+++ src/main/java/org/example/ConTroller/TaophiController.java
@@ -2,16 +2,23 @@ package org.example.ConTroller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.EntityAll.DanhSachKhoanPhi;
 import org.example.Hibernatedao.DanhSachKhoanPhiDao;
 
+import java.net.URL;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-public class TaophiController {
+public class TaophiController implements Initializable {
+    @FXML
+    private Button buttontaomoi;
 
     @FXML
     private TextField loaikhoanphi;
@@ -22,7 +29,7 @@ public class TaophiController {
     @FXML
     private TextField tenkhoanphi;
     @FXML
-    private DatePicker hannop;
+    private TextField hannop;
     @FXML
     void huy(ActionEvent event) {
         Stage a = (Stage) sotien.getScene().getWindow();
@@ -36,12 +43,32 @@ public class TaophiController {
         danhSachKhoanPhi.setTenKhoanPhi(tenkhoanphi.getText());
         LocalDate currentDate = LocalDate.now();
         danhSachKhoanPhi.setBatDau(Date.valueOf(currentDate));
-        danhSachKhoanPhi.setKetThuc(Date.valueOf(hannop.getValue()));
-
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String date= hannop.getText();
+        LocalDate datetime = LocalDate.parse(date, formatter);
+        danhSachKhoanPhi.setKetThuc(Date.valueOf(datetime));
         danhSachKhoanPhi.setGiaTri(Double.parseDouble(sotien.getText()));
         DanhSachKhoanPhiDao.getInstance().save(danhSachKhoanPhi);
         Stage a = (Stage) sotien.getScene().getWindow();
         a.close();
+    }
+    private void checkAllFieldsFilled(TextField[] textFields) {
+        boolean allFilled = true;
+        for (TextField textField : textFields) {
+            if (textField.getText().isEmpty()) {
+                allFilled = false;
+                break;
+            }
+        }
+
+        buttontaomoi.setDisable(!allFilled);
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TextField[] textFields={tenkhoanphi,loaikhoanphi,sotien,hannop};
+        for (TextField textField : textFields) {
+            textField.textProperty().addListener((observable, oldValue, newValue) -> checkAllFieldsFilled(textFields));
+        }
+
     }
 }
