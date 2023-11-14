4e2da555740e4439edcee8ab05790c30e9ee4e55
J Johnson <dung060903@gmail.com>
2023-11-14 19:05:12 +0700

    synchronicity

--- src/main/java/org/example/EntityAll/NopPhi.java
+++ src/main/java/org/example/EntityAll/NopPhi.java
@@ -25,16 +25,21 @@ public class NopPhi {
     @Column(name = "ngay_nop_phi")
     private Date ngayNopPhi;
 
+    @Column(name = "so_tien_da_dong")
+    private double soTienDaDong;
+
 
     public NopPhi() {
         // Default constructor required by Hibernate
     }
 
-    public NopPhi(DanhSachKhoanPhi idKhoanPhi, HoKhau hoKhau, boolean trangThaiDongPhi, Date ngayNopPhi) {
+    public NopPhi(int id, DanhSachKhoanPhi idKhoanPhi, HoKhau hoKhau, boolean trangThaiDongPhi, Date ngayNopPhi, double soTienDaDong) {
+        this.id = id;
         this.idKhoanPhi = idKhoanPhi;
         this.hoKhau = hoKhau;
         this.trangThaiDongPhi = trangThaiDongPhi;
         this.ngayNopPhi = ngayNopPhi;
+        this.soTienDaDong = soTienDaDong;
     }
 
     public int getId() {
@@ -69,4 +74,11 @@ public class NopPhi {
         this.trangThaiDongPhi = trangThaiDongPhi;
     }
 
+    public double getSoTienDaDong() {
+        return soTienDaDong;
+    }
+
+    public void setSoTienDaDong(double soTienDaDong) {
+        this.soTienDaDong = soTienDaDong;
+    }
 }
