3569d9969c9a21309210b422ddf6cbf37146300d
hoang-mai <maianhhoang31072003@gmail.com>
2023-11-24 15:10:37 +0700

    lam chill 16

---
+++ src/main/java/org/example/ConTroller/Taomoihokhau.java
@@ -0,0 +1,47 @@
+package org.example.ConTroller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Taomoihokhau {
+
+        @FXML
+        private Button buttontaomoihokhau;
+
+        @FXML
+        private TextField dientichphong;
+
+        @FXML
+        private TextField gioitinh;
+
+        @FXML
+        private TextField ngaysinh;
+
+        @FXML
+        private TextField quoctich;
+
+        @FXML
+        private TextField sophong;
+
+        @FXML
+        private TextField sotang;
+
+        @FXML
+        private TextField tenchuho;
+
+        @FXML
+        void huy(ActionEvent event) {
+
+        }
+
+        @FXML
+        void taomoi(ActionEvent event) {
+
+        }
+
+    }
+
+}
