ca69badea2670c38d0d1d650c296fac5510cb616
hoang-mai <maianhhoang31072003@gmail.com>
2023-11-26 18:16:32 +0700

    lam chill 16

--- src/main/java/org/example/getData.java
+++ src/main/java/org/example/getData.java
@@ -6,11 +6,12 @@ import org.example.Hibernatedao.HoKhauDao;
 import org.example.Hibernatedao.NhanKhauDao;
 
 import java.util.List;
+import java.util.Map;
 
 public class getData {
     private static getData instance;
     private List<HoKhau> hoKhaus;
-    private List<NhanKhau> nhanKhaus;
+    private Map<Integer,NhanKhau> nhanKhaus;
 
     private getData() {
         // Khởi tạo dữ liệu khi lớp được tạo ra
@@ -25,14 +26,17 @@ public class getData {
         return instance;
     }
 
-    public List<NhanKhau> getNhanKhaus() {
+    public Map<Integer,NhanKhau> getNhanKhaus() {
         return nhanKhaus;
     }
     public void addNhankhau(NhanKhau nhanKhau){
-        nhanKhaus.add(nhanKhau);
+        nhanKhaus.put(nhanKhau.getIdNguoiDan(), nhanKhau);
     }
     public void removeNhankhau(NhanKhau nhanKhau){
-        nhanKhaus.remove(nhanKhau);
+        nhanKhaus.remove(nhanKhau.getIdNguoiDan());
+    }
+    public void setNhankhau(NhanKhau nhanKhau){
+        nhanKhaus.put(nhanKhau.getIdNguoiDan(), nhanKhau);
     }
 
     public List<HoKhau> getHoKhaus() {
@@ -48,7 +52,7 @@ public class getData {
     public void reloadNhankhau() {
         // Làm mới dữ liệu từ HoKhauDao và NhanKhauDao
 
-        this.nhanKhaus = NhanKhauDao.getInstance().selectAll();
+        this.nhanKhaus = NhanKhauDao.getInstance().selectReturnMap();
     }
     public void reloadHokhau(){
         this.hoKhaus = HoKhauDao.getInstance().selectAll();
