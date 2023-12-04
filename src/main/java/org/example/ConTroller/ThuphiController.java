aaaf3626090c241ac8d9229ffdca9795e5528074
pinocchioo1412 <oulive1412@gmail.com>
2023-12-04 09:44:26 +0700

    can we date???

---
+++ src/main/java/org/example/ConTroller/ThuphiController.java
@@ -0,0 +1,60 @@
+package org.example.ConTroller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+public class ThuphiController {
+    @FXML
+    private Label duno;
+
+    @FXML
+    private ListView<?> listviewtenphi;
+
+    @FXML
+    private ListView<?> listviewtenphong;
+
+    @FXML
+    private Label loaikhoanphi;
+
+    @FXML
+    private TextField sophong;
+
+    @FXML
+    private Label sotien;
+
+    @FXML
+    private Label sotiendanop;
+
+    @FXML
+    private TextField sotiennop;
+
+    @FXML
+    private TextField tenphi;
+
+    @FXML
+    void listviewtenphi(MouseEvent event) {
+
+    }
+
+    @FXML
+    void listviewtenphong(MouseEvent event) {
+
+    }
+
+    @FXML
+    void nopphi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void timphi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void timphong(ActionEvent event) {
+
+    }
+}
