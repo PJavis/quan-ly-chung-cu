11c288d47b6c415e2b7814c1f76125bcf38191d3
pinocchioo1412 <oulive1412@gmail.com>
2023-12-18 09:47:18 +0700

    can we date???

--- src/main/java/org/example/ConTroller/HboxController.java
+++ src/main/java/org/example/ConTroller/HboxController.java
@@ -1,5 +1,39 @@
 package org.example.ConTroller;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.example.getData;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HboxController {
+public class HboxController implements Initializable {
+    @FXML
+    private Button chitiet;
 
+    @FXML
+    private Label dientich;
+
+    @FXML
+    private Label sonhankhau;
+
+    @FXML
+    private Label sophong;
+
+    @FXML
+    private Label tenchuho;
+
+    @FXML
+    private Label tienthue;
+    //dang lam gio
+//    public void setChitiet(){
+//        this.sophong.setText(String.valueOf(getData.getInstance().get));
+//        this.tenchuho
+//        this.sonhankhau
+//        this.tienthue
+//        this.dientich
+//    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
--- src/main/java/org/example/ConTroller/TrangchuController.java
+++ src/main/java/org/example/ConTroller/TrangchuController.java
@@ -4,14 +4,47 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import org.example.getData;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
-public class TrangchuController  {
+public class TrangchuController implements Initializable {
 
     @FXML
     private VBox cacitem;
+    @FXML
+    private Label sophongcontrong = new Label(String.valueOf(getData.getInstance().getHoKhaus().size()));
+
+    @FXML
+    private Label tongsonguoio;
+
+    @FXML
+    private Label tongsotienthuduoc;
+    public void setTongsonguoio(){
+        this.tongsonguoio.setText(String.valueOf(getData.getInstance().getNhanKhaus().size()));
+    }
+    public void setSophongcontrong(){
+        this.sophongcontrong.setText(String.valueOf(1000-getData.getInstance().getHoKhaus().size()));
+    }
+    public void setTongsotienthuduoc(){
+        this.tongsotienthuduoc.setText(String.valueOf(10000000*getData.getInstance().getHoKhaus().size()));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setTongsonguoio();
+        setSophongcontrong();
+        setTongsotienthuduoc();
+    }
+//    public  String getDecimalFormatsotien(){
+//        int giatri = getData.getInstance().getHoKhaus().size()*10000000;
+//        String pattern = "#,##0" + (String.valueOf(giatri)) % 1 == 0 ? "" : ".#########");
+//        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+//        return decimalFormat.format(giatri);
+//    }
 }
