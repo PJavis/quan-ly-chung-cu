cbd0c264a8a44ef207d77acd03488ded098073c9
hoang-mai <maianhhoang31072003@gmail.com>
2023-12-22 19:52:48 +0700

    lam chill 16

--- src/main/java/org/example/ConTroller/Tracuulichsugiaodich.java
+++ src/main/java/org/example/ConTroller/Tracuulichsugiaodich.java
@@ -1,4 +1,58 @@
 package org.example.ConTroller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.example.EntityAll.LichSuGiaoDich;
+import org.example.EntityAll.NopPhi;
+import org.example.Hibernatedao.LichSuGiaoDichDao;
+
+import java.util.List;
+
 public class Tracuulichsugiaodich {
+
+    @FXML
+    private DatePicker den;
+
+
+    @FXML
+    private DatePicker tu;
+
+    @FXML
+    private TableColumn<LichSuGiaoDich, String> nguoinoptien;
+
+    @FXML
+    private TableColumn<LichSuGiaoDich, Integer> sothutu;
+
+    @FXML
+    private TableColumn<LichSuGiaoDich, Double> sotiennop;
+
+    @FXML
+    private TableColumn<LichSuGiaoDich, String> thoigiannop;
+
+    @FXML
+    private TableView<LichSuGiaoDich> danhsachgiaodich;
+
+
+    private List<LichSuGiaoDich> lichSuGiaoDichList;
+    private ObservableList<LichSuGiaoDich> lichSuGiaoDichObservableList;
+    public void danhsachgiaodich(){
+        lichSuGiaoDichObservableList= FXCollections.observableArrayList(lichSuGiaoDichList);
+        sothutu.setCellValueFactory(cellData -> {
+            int rowIndex = cellData.getTableView().getItems().indexOf(cellData.getValue()) + 1;
+            return javafx.beans.binding.Bindings.createObjectBinding(() -> rowIndex);
+        });
+        nguoinoptien.setCellValueFactory(new PropertyValueFactory<>("tennguoinop"));
+        sotiennop.setCellValueFactory(new PropertyValueFactory<>("decimalFormatsotiennop"));
+        thoigiannop.setCellValueFactory(new PropertyValueFactory<>("formattedDate"));
+        danhsachgiaodich.setItems(lichSuGiaoDichObservableList);
+    }
+
+    @FXML
+    void tim(ActionEvent event) {
+
+    }
 }
