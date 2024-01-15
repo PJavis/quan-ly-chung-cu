8f6d82824cfacc384f189266ae96d517c2c0f1c0
pinocchioo1412 <oulive1412@gmail.com>
2024-01-15 07:48:53 +0700

    pinocchioLHP

--- src/main/java/org/example/ConTroller/Quanlyphong.java
+++ src/main/java/org/example/ConTroller/Quanlyphong.java
@@ -249,18 +249,18 @@ public class Quanlyphong implements Initializable {
     }
     private String laytenthang(int month) {
         switch (month) {
-            case 1: return "January";
-            case 2: return "February";
-            case 3: return "March";
-            case 4: return "April";
-            case 5: return "May";
-            case 6: return "June";
-            case 7: return "July";
-            case 8: return "August";
-            case 9: return "September";
-            case 10: return "October";
-            case 11: return "November";
-            case 12: return "December";
+            case 1: return "Tháng 1";
+            case 2: return "Tháng 2";
+            case 3: return "Tháng 3";
+            case 4: return "Tháng 4";
+            case 5: return "Tháng 5";
+            case 6: return "Tháng 6";
+            case 7: return "Tháng 7";
+            case 8: return "Tháng 8";
+            case 9: return "Tháng 9";
+            case 10: return "Tháng 10";
+            case 11: return "Tháng 11";
+            case 12: return "Tháng 12";
             default: return "Unknown";
         }
     }
